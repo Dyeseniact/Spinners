1450840c57ea3d691d023a5f19d007331af0c374
Dyeseniact <camposdyes@gmail.com>
2021-07-27 20:35:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/spinners/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.spinners
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+
+    var language = arrayOf("Inglés", "Español", "Chino", "Ruso", "Coreano", "Alemán", "Francés", "Holandés")
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var spinner = findViewById<Spinner>(R.id.spinner)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                mostrar("No seleccionaste idioma","Vuelve a desplegar la lista y asegúrate de elegir correctamente a alguna")
+                Toast.makeText(applicationContext, "No idioma" , Toast.LENGTH_LONG).show()
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(applicationContext ,language[position] , Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        val languageAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, language)
+
+        languageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = languageAdapter
+
+    }
+        private fun mostrar(title:String,message:String){
+            AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK"){dialogInterface, which -> }
+                .create()
+                .show()
+    }
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                      }
+
